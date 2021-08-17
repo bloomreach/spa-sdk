2c2ee3b7b501bd1dfd884498a773c705deaf5a0a
Vasili Kauryha <vasili.kauryha@bloomreach.com>
2021-08-17 16:13:21 +0200

    SPASDK-13 Move all eslint config to package.json except angular projects
    Angular configs are kept in the `.eslintrc.js` to provide `tsconfigRootDir`
    property value `__dirname` to solve the problem with IDE eslint.

--- examples/angular/.eslintrc.js
+++ examples/angular/.eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
-    "root": true,
-    "extends": "@bloomreach/eslint-config-angular",
-    "parserOptions": {
-      "project": [
+    root: true,
+    extends: [
+      "@bloomreach/eslint-config-angular"
+    ],
+    parserOptions: {
+      project: [
         "tsconfig.json",
         "tsconfig.spec.json",
       ],
       tsconfigRootDir: __dirname,
-      "createDefaultProgram": true
+      createDefaultProgram: true
     }
   }
--- packages/ng-sdk/.eslintrc.js
+++ packages/ng-sdk/.eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "root": true,
-  "extends": "@bloomreach/eslint-config-angular",
-  "parserOptions": {
-    "project": [
+  extends: [
+    "@bloomreach/eslint-config-angular"
+  ],
+  parserOptions: {
+    project: [
       "tsconfig.json",
       "tsconfig.spec.json",
     ],
     tsconfigRootDir: __dirname,
-    "createDefaultProgram": true
+    createDefaultProgram: true
   }
 }
