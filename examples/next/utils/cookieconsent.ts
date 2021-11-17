ba63be5fa83435ea0976bed66e4007213322a6c3
Vasili Kauryha <vasili.kauryha@bloomreach.com>
2021-11-17 14:24:21 +0100

    SPASDK-72 Run only campaign personalization if exponea token and url are absent
    * Prevent runnig Segmentation JS lib on cookieconsent init because it already called
    during the component render;
    * Inject exponea sdk snippet only when token and url are present;
    * Remove code by setting empty string for env variables;

--- examples/next/utils/cookieconsent.ts
+++ examples/next/utils/cookieconsent.ts
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import { initializePersonalization } from '@bloomreach/segmentation';
+import { initializePersonalization, initializeCampaignPersonalization } from '@bloomreach/segmentation';
 
 declare global {
   interface Window {
@@ -23,8 +23,8 @@ declare global {
 
 const COOKIE_CONSENTS_EXPIRATION_VALUE = 28; // Days
 
-const projectToken = process.env.NEXT_PUBLIC_EXPONEA_PROJECT_TOKEN ?? '';
-const apiUrl = process.env.NEXT_PUBLIC_EXPONEA_API_URL ?? '';
+const exponeaProjectToken = process.env.NEXT_PUBLIC_EXPONEA_PROJECT_TOKEN;
+const exponeaApiUrl = process.env.NEXT_PUBLIC_EXPONEA_API_URL;
 
 const injectScript = (scriptContent: string): void => {
   const scriptTag = document.createElement('script');
@@ -32,29 +32,38 @@ const injectScript = (scriptContent: string): void => {
   document.head.appendChild(scriptTag);
 };
 
-const exponeaSdkSnippet = `
+const getExponeaSdkSnippet = (token: string, apiUrl: string): string => `
   !function(e,n,t,i,o,r){function c(e){if("number"!=typeof e)return e;var n=new Date;return new Date(n.getTime()+1e3*e)}var a=4e3,s="xnpe_async_hide";function p(e){return e.reduce((function(e,n){return e[n]=function(){e._.push([n.toString(),arguments])},e}),{_:[]})}function m(e,n,t){var i=t.createElement(n);i.src=e;var o=t.getElementsByTagName(n)[0];return o.parentNode.insertBefore(i,o),i}function u(e){return"[object Date]"===Object.prototype.toString.call(e)}r.target=r.target||"https://api.exponea.com",r.file_path=r.file_path||r.target+"/js/exponea.min.js",o[n]=p(["anonymize","initialize","identify","update","track","trackLink","trackEnhancedEcommerce","getHtml","showHtml","showBanner","showWebLayer","ping","getAbTest","loadDependency","getRecommendation","reloadWebLayers"]),o[n].notifications=p(["isAvailable","isSubscribed","subscribe","unsubscribe"]),o[n]["snippetVersion"]="v2.3.0",function(e,n,t){e[n]["_"+t]={},e[n]["_"+t].nowFn=Date.now,e[n]["_"+t].snippetStartTime=e[n]["_"+t].nowFn()}(o,n,"performance"),function(e,n,t,i,o,r){e[o]={sdk:e[i],sdkObjectName:i,skipExperiments:!!t.new_experiments,sign:t.token+"/"+(r.exec(n.cookie)||["","new"])[1],path:t.target}}(o,e,r,n,i,RegExp("__exponea_etc__"+"=([\\w-]+)")),function(e,n,t){m(e.file_path,n,t)}(r,t,e),function(e,n,t,i,o,r,p){if(e.new_experiments){!0===e.new_experiments&&(e.new_experiments={});var f,l=e.new_experiments.hide_class||s,_=e.new_experiments.timeout||a,d=encodeURIComponent(r.location.href.split("#")[0]);e.cookies&&e.cookies.expires&&("number"==typeof e.cookies.expires||u(e.cookies.expires)?f=c(e.cookies.expires):e.cookies.expires.tracking&&("number"==typeof e.cookies.expires.tracking||u(e.cookies.expires.tracking))&&(f=c(e.cookies.expires.tracking))),f&&f<new Date&&(f=void 0);var x=e.target+"/webxp/"+n+"/"+r[t].sign+"/modifications.min.js?http-referer="+d+"&timeout="+_+"ms"+(f?"&cookie-expires="+Math.floor(f.getTime()/1e3):"");"sync"===e.new_experiments.mode&&r.localStorage.getItem("__exponea__sync_modifications__")?function(e,n,t,i,o){t[o][n]="<"+n+' src="'+e+'"></'+n+">",i.writeln(t[o][n]),i.writeln("<"+n+">!"+o+".init && document.writeln("+o+"."+n+'.replace("/'+n+'/", "/'+n+'-async/").replace("><", " async><"))</'+n+">")}(x,n,r,p,t):function(e,n,t,i,o,r,c,a){r.documentElement.classList.add(e);var s=m(t,i,r);function p(){o[a].init||m(t.replace("/"+i+"/","/"+i+"-async/"),i,r)}function u(){r.documentElement.classList.remove(e)}s.onload=p,s.onerror=p,o.setTimeout(u,n),o[c]._revealPage=u}(l,_,x,n,r,p,o,t)}}(r,t,i,0,n,o,e),function(e,n,t){e[n].start=function(i){i&&Object.keys(i).forEach((function(e){return t[e]=i[e]})),e[n].initialize(t)}}(o,n,r)}(document,"exponea","script","webxpClient",window,{
       target: "${apiUrl}",
-      token: "${projectToken}",
+      token: "${token}",
       // replace with current customer ID or leave commented out for an anonymous customer
       // customer: window.currentUserId,
   });
   exponea.start();
 `;
 
-export const runPersonalization = (path = '/'): void => {
-  initializePersonalization({ projectToken, targetURL: apiUrl, path });
-};
+const setupPersonalization =
+  (token?: string, apiUrl?: string) =>
+  (path = '/'): void => {
+    if (token && apiUrl) {
+      initializePersonalization({ projectToken: token, targetURL: apiUrl, path });
+    } else {
+      initializeCampaignPersonalization({ path });
+    }
+  };
 
-const runExponeaCookie = (path: string): void => {
-  injectScript(exponeaSdkSnippet);
-  runPersonalization(path);
-};
+export const runPersonalization = setupPersonalization(exponeaProjectToken, exponeaApiUrl);
 
 export const isConsentReceived = (): boolean => {
-  const { cookieconsent } = window;
+  const { cookieconsent } = window || {};
 
   return !!cookieconsent && cookieconsent.utils.getCookie('cookieconsent_status') === cookieconsent.status.allow;
+};
+
+const injectExponeaScriptSnippet = (token?: string, apiUrl?: string): void => {
+  if (!token || !apiUrl) return;
+
+  injectScript(getExponeaSdkSnippet(token, apiUrl));
 };
 
 const CookieConsentInit = (): void => {
@@ -74,12 +83,13 @@ const CookieConsentInit = (): void => {
     type: 'opt-in',
     onInitialise: (status) => {
       if (status === window.cookieconsent?.status.allow) {
-        runExponeaCookie(`${window.location.pathname}${window.location.search}`);
+        injectExponeaScriptSnippet(exponeaProjectToken, exponeaApiUrl);
       }
     },
     onStatusChange: (status) => {
       if (status === window.cookieconsent?.status.allow) {
-        runExponeaCookie(`${window.location.pathname}${window.location.search}`);
+        injectExponeaScriptSnippet(exponeaProjectToken, exponeaApiUrl);
+        runPersonalization(`${window.location.pathname}${window.location.search}`);
       }
     },
   });
