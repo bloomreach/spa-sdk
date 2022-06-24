b3eb33f898684fbeb62b43db67ef458b82dde76f
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2022-06-24 14:11:14 +0200

    SPASDK-119 Improve heroku deploy speed by using lerna filter flags
    Lerna can filter the packages it is running commands on via its build in
    filter flags. In this case we can use the `--scope` flag and the
    `--include-dependencies` flag to only build our to-be-deployed app and
    its dependencies. E.g.: the example react app, the react sdk and the spa
    sdk.

--- scripts/deploy_heroku_app.sh
+++ scripts/deploy_heroku_app.sh
@@ -24,7 +24,9 @@ then
   APP_PATH="examples/react"
 fi
 
-echo "Deploying ${NAME} app using path ${APP_PATH}";
+APP_PACKAGE=$(node -p -e "require('./$APP_PATH/package.json').name")
+
+echo "Deploying ${APP_PACKAGE} as ${NAME} app using path ${APP_PATH}";
 echo '-----------------------------------------------------------------------------'
 
 heroku plugins:install buildpack-registry
@@ -49,7 +51,7 @@ fi
 # Set common config options
 heroku config:set --app=$NAME PROJECT_PATH=$APP_PATH
 heroku config:set --app=$NAME PROCFILE=$APP_PATH/Procfile
-
+heroku config:set --app=$NAME PACKAGE=$APP_PACKAGE
 
 # Set project specific config options
 if [[ $APP_TYPE = "ssr" ]] && [[ $APP_NAME = "ng" ]]
