81316945f49fe34336a5330e09c46b755be31f3d
Nikola Trajkovski <nikola.trajkovski@bloomreach.com>
2022-11-08 10:21:07 +0000

    TRIVIAL Update the description for the cleanup utility

--- scripts/cleanup_heroku_apps.sh
+++ scripts/cleanup_heroku_apps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Utility that deletes all heroku apps with a versions lower than the provided version
+# Utility that prompts you to delete the example apps hosted on Heroku. It goes through all the apps that match a pattern and asks if that app should be deleted.
 
 set -e
 
@@ -8,11 +8,11 @@ HEROKU_TEAM="bloomreach"
 for line in $( heroku apps --team ${HEROKU_TEAM} | grep '^\(ng\|react\|vue\)' ); do
   echo "Would you like to delete app $line ?  [Y] Yes  [any key] No     "
   read -sn1 input
-    if [ "$input" = "Y" ]; 
+    if [ "$input" = "Y" ];
     then
         echo "Yes"
         heroku apps:destroy --app=${line} --confirm ${line} || true
-    else 
+    else
         echo "No"
     fi
 done
