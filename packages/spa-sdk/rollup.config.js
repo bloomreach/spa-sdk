dea5b49802de8398a230fc73a7127f13125e36ac
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2022-11-30 23:42:13 -0800

    SPASDK-148 Output sourcemaps for ES modules

--- packages/spa-sdk/rollup.config.js
+++ packages/spa-sdk/rollup.config.js
@@ -57,6 +57,7 @@ export default [
         dir: 'lib',
         entryFileNames: '[name].js',
         format: 'es',
+        sourcemap: true,
       },
     ],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
