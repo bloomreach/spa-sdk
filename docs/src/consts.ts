ceb66014f9a8c62dabab782175c15fff85a73c47
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2023-07-30 22:20:09 +0200

    SPASDK-194 Strip language support from the project

--- docs/src/consts.ts
+++ docs/src/consts.ts
@@ -1,8 +1,8 @@
 export const SITE = {
 	title: 'Bloomreach SPA SDKs',
 	description: `Bloomreach provides an SPA SDK for integrating SPAs and other Javascript front-end applications with Bloomreach Content's Delivery API and the preview and editing capabilities of the Experience manager application.`,
-	defaultLanguage: 'en-us',
 } as const;
+
 
 export const OPEN_GRAPH = {
 	image: {
@@ -13,11 +13,6 @@ export const OPEN_GRAPH = {
 	},
 	twitter: 'astrodotbuild',
 };
-
-export const KNOWN_LANGUAGES = {
-	English: 'en',
-} as const;
-export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
 export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
 
@@ -30,31 +25,31 @@ export const ALGOLIA = {
 	apiKey: 'XXXXXXXXXX',
 };
 
-export type Sidebar = Record<
-	(typeof KNOWN_LANGUAGE_CODES)[number],
-	Record<string, { text: string; link: string }[]>
->;
+type MenuItem = { text: string; link: string; }
+export type Sidebar = Record<string, MenuItem[]>
+
 export const SIDEBAR: Sidebar = {
-	en: {
-		'Content SaaS': [
-			{ text: 'Getting started', link: '/en/getting-started' },
-			{ text: 'Configuration', link: '/en/configuration' },
-			{ text: 'React SPA SDK', link: '/en/react' },
-			{ text: 'Angular SPA SDK', link: '/en/angular' },
-			{ text: 'Vue 2 SPA SDK', link: '/en/vue-2' },
-			{ text: 'Vue 3 SPA SDK', link: '/en/vue-3' },
-			{ text: 'Framework agnostic SPA SDK', link: '/en/spa-sdk' },
-			{ text: 'Internal reference', link: '/en/reference-saas' },
-		],
-		'Content PaaS': [
-			{ text: 'Relevance integration', link: '/en/relevance-integration' },
-			{ text: 'Internal reference', link: '/en/reference-paas' },
-		],
-		'Migration': [
-			{ text: 'v19', link: '/en/v19' },
-			{ text: 'v18', link: '/en/v18' },
-			{ text: 'v17', link: '/en/v17' },
-			{ text: 'v15', link: '/en/v15' },
-		],
-	},
+	'Getting started': [
+		{ text: 'Getting started', link: '/getting-started' },
+		{ text: 'Configuration', link: '/configuration' },
+	],
+	'SDK usage': [
+		{ text: 'SPA SDK', link: '/spa-sdk' },
+		{ text: 'React SDK', link: '/react' },
+		{ text: 'Angular SDK', link: '/angular' },
+		{ text: 'Vue 2 SDK', link: '/vue-2' },
+		{ text: 'Vue 3 SDK', link: '/vue-3' },
+	],
+	'Migration': [
+		{ text: 'Migration guide', link: '/migration-guide' },
+		{ text: 'Release notes', link: '/release-notes' },
+		{ text: 'Changelog', link: '/changelog' },
+	],
+	'Content SaaS': [
+		{ text: 'Internal reference', link: '/reference-saas' },
+	],
+	'Content PaaS': [
+		{ text: 'Relevance integration', link: '/relevance-integration' },
+		{ text: 'Internal reference', link: '/reference-paas' },
+	],
 };
