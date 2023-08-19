4cb335887cec3893d491b1dc598f7498ef43fef6
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2023-08-18 19:50:22 -0700

    SPASDK-193 Revamp ThemeToggle

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -4,12 +4,24 @@ export default {
   daisyui: {
     themes: [
       {
-        mytheme: {
+        light: {
           primary: '#23BFF2',
           secondary: '#FFE45C',
           accent: '#f471b5',
           neutral: '#BDCEE3',
           'base-100': '#ffffff',
+          info: '#23BFF2',
+          success: '#0DCA7A',
+          warning: '#FF9500',
+          error: '#F24165',
+        },
+        dark: {
+          primary: '#23BFF2',
+          secondary: '#FFE45C',
+          accent: '#f471b5',
+          neutral: '#BDCEE3',
+          'base-100': '#002840',
+          'base-content': '#ffffff',
           info: '#23BFF2',
           success: '#0DCA7A',
           warning: '#FF9500',
