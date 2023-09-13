02440282749e569f1fef8fb9dd1cdf478e174753
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2023-09-13 15:44:51 -0700

    TRIVIAL Clarify variable in heroku deploy script

--- scripts/deploy_heroku_app.sh
+++ scripts/deploy_heroku_app.sh
@@ -4,29 +4,34 @@ set -e
 
 HEROKU_TEAM="bloomreach"
 APP_TYPE=$2;
-APP_NAME=$3;
+APP_FRAMEWORK=$3;
 APP_VERSION=$4;
-NAME=${5:-"${APP_NAME}-${APP_TYPE}-${APP_VERSION}"};
+NAME=${5:-"${APP_FRAMEWORK}-${APP_TYPE}-${APP_VERSION}"};
 
-if [[ $APP_NAME = "ng" ]]
+echo "type ${APP_TYPE}"
+echo "framework ${APP_FRAMEWORK}"
+echo "version ${APP_VERSION}"
+echo "name ${NAME}"
+
+if [[ $APP_FRAMEWORK = "ng" ]]
 then
   APP_PATH="examples/angular"
-elif [[ $APP_TYPE = "ssr" ]] && [[ $APP_NAME = "vue" ]]
+elif [[ $APP_TYPE = "ssr" ]] && [[ $APP_FRAMEWORK = "vue" ]]
 then
   APP_PATH="examples/nuxt"
-elif [[ $APP_TYPE = "ssr" ]] && [[ $APP_NAME = "react" ]]
+elif [[ $APP_TYPE = "ssr" ]] && [[ $APP_FRAMEWORK = "react" ]]
 then
   APP_PATH="examples/next"
-elif [[ $APP_TYPE = "csr" ]] && [[ $APP_NAME = "vue" ]]
+elif [[ $APP_TYPE = "csr" ]] && [[ $APP_FRAMEWORK = "vue" ]]
 then
   APP_PATH="examples/vue"
-elif [[ $APP_TYPE = "csr" ]] && [[ $APP_NAME = "react" ]]
+elif [[ $APP_TYPE = "csr" ]] && [[ $APP_FRAMEWORK = "react" ]]
 then
   APP_PATH="examples/react"
-elif [[ $APP_TYPE = "csr" ]] && [[ $APP_NAME = "vue3" ]]
+elif [[ $APP_TYPE = "csr" ]] && [[ $APP_FRAMEWORK = "vue3" ]]
 then
   APP_PATH="examples/vue3"
-elif [[ $APP_TYPE = "ssr" ]] && [[ $APP_NAME = "vue3" ]]
+elif [[ $APP_TYPE = "ssr" ]] && [[ $APP_FRAMEWORK = "vue3" ]]
 then
 # We don't have vue3 ssr app, but the deployment matrix is done in such a way that it will try to deploy the vue3 ssr
 # So we ignore this until we add ssr app to Heroku
@@ -57,42 +62,42 @@ fi
 # Set common config options
 heroku config:set --app=$NAME NPM_CONFIG_PRODUCTION=false \
   PROJECT_PATH=$APP_PATH \
-  SDK_NAME=$APP_NAME \
+  SDK_NAME=$APP_FRAMEWORK \
   PACKAGE=$APP_PACKAGE \
   PROCFILE=$APP_PATH/Procfile
 
 # Set project specific config options
-if [[ $APP_NAME = "ng" ]]
+if [[ $APP_FRAMEWORK = "ng" ]]
 then
   heroku config:set --app=$NAME EXAMPLE_NAME="angular" BR_MULTI_TENANT_SUPPORT=true
 fi
 
-if [[ $APP_TYPE = "ssr" ]] && [[ $APP_NAME = "ng" ]]
+if [[ $APP_TYPE = "ssr" ]] && [[ $APP_FRAMEWORK = "ng" ]]
 then
   heroku config:set --app=$NAME PROCFILE=$APP_PATH/universal.Procfile
 fi
 
-if [[ $APP_TYPE = "csr" ]] && [[ $APP_NAME = "react" ]]
+if [[ $APP_TYPE = "csr" ]] && [[ $APP_FRAMEWORK = "react" ]]
 then
   heroku config:set --app=$NAME EXAMPLE_NAME="react" REACT_APP_BR_MULTI_TENANT_SUPPORT=true
 fi
 
-if [[ $APP_TYPE = "ssr" ]] && [[ $APP_NAME = "react" ]]
+if [[ $APP_TYPE = "ssr" ]] && [[ $APP_FRAMEWORK = "react" ]]
 then
   heroku config:set --app=$NAME EXAMPLE_NAME="next" NEXT_PUBLIC_BR_MULTI_TENANT_SUPPORT=true
 fi
 
-if [[ $APP_TYPE = "csr" ]] && [[ $APP_NAME = "vue" ]]
+if [[ $APP_TYPE = "csr" ]] && [[ $APP_FRAMEWORK = "vue" ]]
 then
   heroku config:set --app=$NAME EXAMPLE_NAME="vue" VUE_APP_BR_MULTI_TENANT_SUPPORT=true
 fi
 
-if [[ $APP_TYPE = "ssr" ]] && [[ $APP_NAME = "vue" ]]
+if [[ $APP_TYPE = "ssr" ]] && [[ $APP_FRAMEWORK = "vue" ]]
 then
   heroku config:set --app=$NAME EXAMPLE_NAME="nuxt" NUXT_APP_BR_MULTI_TENANT_SUPPORT=true HOST=0.0.0.0
 fi
 
-if [[ $APP_TYPE = "csr" ]] && [[ $APP_NAME = "vue3" ]]
+if [[ $APP_TYPE = "csr" ]] && [[ $APP_FRAMEWORK = "vue3" ]]
 then
   heroku config:set --app=$NAME EXAMPLE_NAME="vue3" VITE_MULTI_TENANT_SUPPORT=true
 fi
