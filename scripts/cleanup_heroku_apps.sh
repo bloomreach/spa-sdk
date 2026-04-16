8badd90dcbd5bbe5162e15ef76eb714c24519416
ksalic <kenan.salic@bloomreach.com>
2026-04-16 10:00:41 +0200

    SPASDK-261 fix: add nginx config and cleanup script
    Address MR !236 review feedback from Charlie:
    
    - Add config/nginx.conf.erb for Heroku nginx buildpack
      with SPA fallback routing and dist/ as root
    - Add lit to cleanup_heroku_apps.sh grep pattern
    
    See also: SPASDK-261

--- scripts/cleanup_heroku_apps.sh
+++ scripts/cleanup_heroku_apps.sh
@@ -5,7 +5,7 @@ set -e
 
 HEROKU_TEAM="bloomreach"
 
-for line in $( heroku apps --team ${HEROKU_TEAM} | grep '^\(ng\|react\|vue\)' ); do
+for line in $( heroku apps --team ${HEROKU_TEAM} | grep '^\(ng\|react\|vue\|lit\)' ); do
   echo "Would you like to delete app $line ?  [Y] Yes  [any key] No     "
   read -sn1 input
     if [ "$input" = "Y" ];
